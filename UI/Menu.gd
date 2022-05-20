b0e41603bb353c0a31787f6929d1980ccae35605
paomwattana <prachnachai.mek@student.mahidol.edu>
2022-05-21 00:13:29 +0700

    added menu buttons (not working yet)

---
+++ UI/Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+
+func _on_Quit_pressed():
+	pass # Replace with function body.
+
+
+func _on_Restart_pressed():
+	pass
+	# get world's node and reload, possibly reset
