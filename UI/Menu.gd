b00936e0cb216cb77da2e0ab9dc2d289c1dec377
paomwattana <prachnachai.mek@student.mahidol.edu>
2022-06-07 15:21:21 +0700

    added functioning start menu

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -2,15 +2,46 @@ extends Control
 
 
 
-# Called when the node enters the scene tree for the first time.
+onready var startButton = $StartButton
+onready var tutorialButton = $TutorialButton
+onready var quitButton = $QuitButton
+onready var tutorialBackButton = $TutorialBackButton
+onready var tutorialRect = $TutorialRect
+
+
 func _ready():
-	visible = false
+	tutorialBackButton.visible = false
+	tutorialRect.visible = false
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://World.tscn")
 
 
-func _on_Quit_pressed():
-	pass # Replace with function body.
+func _on_QuitButton_pressed():
+	get_tree().quit()
 
 
-func _on_Restart_pressed():
-	pass
-	# get world's node and reload, possibly reset
+func _on_TutorialButton_pressed():
+	# hide buttons
+	startButton.visible = false
+	tutorialButton.visible = false
+	quitButton.visible = false
+	
+	# show tutorial img
+	tutorialRect.visible = true
+	
+	# show back button
+	tutorialBackButton.visible = true
+
+
+func _on_TutorialBackButton_pressed():
+	# hide back button
+	tutorialBackButton.visible = false
+	
+	# hide tutorial img
+	tutorialRect.visible = false
+	
+	# show other buttons
+	startButton.visible = true
+	tutorialButton.visible = true
+	quitButton.visible = true
